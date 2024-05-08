299b6240f1624b21fcd29f486619ad991b579dc4
STP5940 <sitthipong114@gmail.com>
2024-05-08 14:32:19 +0700

    Update name frontend

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -1,48 +1,64 @@
-import { fileURLToPath } from 'node:url'
-import vuetify from 'vite-plugin-vuetify'
+import { fileURLToPath } from "node:url";
+import vuetify from "vite-plugin-vuetify";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  devtools: { enabled: true },
+  modules: [
+    "@nuxt/ui",
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+    "@vueuse/nuxt",
+    "@nuxtjs/device",
+    "@pinia/nuxt",
+  ],
+  colorMode: {
+    preference: "light",
+  },
   app: {
     head: {
-      titleTemplate: '%s - NuxtJS Admin Template',
-      title: 'Vuexy',
+      titleTemplate: "%s - NuxtJS Admin Template",
+      title: "Vuexy",
 
-      link: [{
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico',
-      }],
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/favicon.ico",
+        },
+      ],
     },
   },
 
-  devtools: {
-    enabled: true,
-  },
-
   css: [
-    '@core/scss/template/index.scss',
-    '@styles/styles.scss',
-    '@/plugins/iconify/icons.css',
+    "@core/scss/template/index.scss",
+    "@styles/styles.scss",
+    "@/plugins/iconify/icons.css",
   ],
 
   components: {
-    dirs: [{
-      path: '@/@core/components',
-      pathPrefix: false,
-    }, {
-      path: '~/components/global',
-      global: true,
-    }, {
-      path: '~/components',
-      pathPrefix: false,
-    }],
+    dirs: [
+      {
+        path: "@/@core/components",
+        pathPrefix: false,
+      },
+      {
+        path: "~/components/global",
+        global: true,
+      },
+      {
+        path: "~/components",
+        pathPrefix: false,
+        extensions: [".js", ".jsx", ".mjs", ".ts", ".vue", ".tsx"],
+      },
+    ],
   },
 
-  plugins: ['@/plugins/vuetify/index.ts', '@/plugins/iconify/index.ts'],
+  plugins: ["@/plugins/vuetify/index.ts", "@/plugins/iconify/index.ts"],
 
   imports: {
-    dirs: ['./@core/utils', './@core/composable/', './plugins/*/composables/*'],
+    dirs: ["./@core/utils", "./@core/composable/", "./plugins/*/composables/*"],
     presets: [],
   },
 
@@ -50,24 +66,23 @@ export default defineNuxtConfig({
 
   experimental: {
     typedPages: true,
-    inlineSSRStyles: false,
   },
 
   typescript: {
     tsConfig: {
       compilerOptions: {
         paths: {
-          '@/*': ['../*'],
-          '@themeConfig': ['../themeConfig.ts'],
-          '@layouts/*': ['../@layouts/*'],
-          '@layouts': ['../@layouts'],
-          '@core/*': ['../@core/*'],
-          '@core': ['../@core'],
-          '@images/*': ['../assets/images/*'],
-          '@styles/*': ['../assets/styles/*'],
-          '@validators': ['../@core/utils/validators'],
-          '@db/*': ['../server/fake-db/*'],
-          '@api-utils/*': ['../server/utils/*'],
+          "@/*": ["../*"],
+          "@themeConfig": ["../themeConfig.ts"],
+          "@layouts/*": ["../@layouts/*"],
+          "@layouts": ["../@layouts"],
+          "@core/*": ["../@core/*"],
+          "@core": ["../@core"],
+          "@images/*": ["../assets/images/*"],
+          "@styles/*": ["../assets/styles/*"],
+          "@validators": ["../@core/utils/validators"],
+          "@db/*": ["../server/fake-db/*"],
+          "@api-utils/*": ["../server/utils/*"],
         },
       },
     },
@@ -81,25 +96,34 @@ export default defineNuxtConfig({
 
   vue: {
     compilerOptions: {
-      isCustomElement: tag => tag === 'swiper-container' || tag === 'swiper-slide',
+      isCustomElement: (tag) =>
+        tag === "swiper-container" || tag === "swiper-slide",
     },
   },
 
   vite: {
-    define: { 'process.env': {} },
+    define: { "process.env": {} },
 
     resolve: {
       alias: {
-        '@': fileURLToPath(new URL('.', import.meta.url)),
-        '@themeConfig': fileURLToPath(new URL('./themeConfig.ts', import.meta.url)),
-        '@core': fileURLToPath(new URL('./@core', import.meta.url)),
-        '@layouts': fileURLToPath(new URL('./@layouts', import.meta.url)),
-        '@images': fileURLToPath(new URL('./assets/images/', import.meta.url)),
-        '@styles': fileURLToPath(new URL('./assets/styles/', import.meta.url)),
-        '@configured-variables': fileURLToPath(new URL('./assets/styles/variables/_template.scss', import.meta.url)),
-        'apexcharts': fileURLToPath(new URL('node_modules/apexcharts-clevision', import.meta.url)),
-        '@db': fileURLToPath(new URL('./server/fake-db/', import.meta.url)),
-        '@api-utils': fileURLToPath(new URL('./server/utils/', import.meta.url)),
+        "@": fileURLToPath(new URL(".", import.meta.url)),
+        "@themeConfig": fileURLToPath(
+          new URL("./themeConfig.ts", import.meta.url),
+        ),
+        "@core": fileURLToPath(new URL("./@core", import.meta.url)),
+        "@layouts": fileURLToPath(new URL("./@layouts", import.meta.url)),
+        "@images": fileURLToPath(new URL("./assets/images/", import.meta.url)),
+        "@styles": fileURLToPath(new URL("./assets/styles/", import.meta.url)),
+        "@configured-variables": fileURLToPath(
+          new URL("./assets/styles/variables/_template.scss", import.meta.url),
+        ),
+        apexcharts: fileURLToPath(
+          new URL("node_modules/apexcharts-clevision", import.meta.url),
+        ),
+        "@db": fileURLToPath(new URL("./server/fake-db/", import.meta.url)),
+        "@api-utils": fileURLToPath(
+          new URL("./server/utils/", import.meta.url),
+        ),
       },
     },
 
@@ -108,16 +132,14 @@ export default defineNuxtConfig({
     },
 
     optimizeDeps: {
-      exclude: ['vuetify'],
-      entries: [
-        './**/*.vue',
-      ],
+      exclude: ["vuetify"],
+      entries: ["./**/*.vue"],
     },
 
     plugins: [
       vuetify({
         styles: {
-          configFile: 'assets/styles/variables/_vuetify.scss',
+          configFile: "assets/styles/variables/_vuetify.scss",
         },
       }),
       null,
@@ -125,8 +147,6 @@ export default defineNuxtConfig({
   },
 
   build: {
-    transpile: ['vuetify'],
+    transpile: ["vuetify"],
   },
-
-  modules: ['@vueuse/nuxt', '@nuxtjs/device', '@pinia/nuxt'],
-})
+});
